43bf44a7a8bf775095ff4cecb77e84dfe405cef8
msalah2024 <ms1912733@qu.edu.qa>
2025-02-01 01:20:22 +0300

    Lawfirm progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ServicesSection from "./components/ServicesSection";
+import LawFirmSection from "./components/LawFirmSection";
 function App() {
     return (
         <div className="home-page">
             <Navbar />
             <Hero />
             <ServicesSection />
+            <LawFirmSection />
         </div>
     );
 }
---
+++ src/components/LawFirmSection.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+import { Typography } from "@mui/material";
+export default function LawFirmSection() {
+    return (
+        <div className="law-firm-section">
+            <Typography
+                variant="body1"
+                sx={{
+                    color: "secondary.main",
+                    fontWeight: "700",
+                    textAlign: "center",
+                }}
+            >
+                LawFirm
+            </Typography>
+            <Typography
+                variant="h4"
+                sx={{
+                    color: "primary.main",
+                    fontWeight: "700",
+                    textAlign: "center",
+                }}
+                >
+                Specialized lawyers around the clock
+            </Typography>
+        </div>
+    );
+}
